4e96fe05299dad88e3c85205843e4c8752a32c92
Tommaso Fornaciari <tommaso.fornaciari@gmail.com>
2021-02-11 14:23:13 +0100

    badge

--- test_boot/test_boostsa.py
+++ test_boot/test_boostsa.py
@@ -1,6 +1,6 @@
 # coding=latin-1
 import sys
-sys.path.append('/home/runner/work/boostsa/boostsa/boostsa')
+sys.path.extend(['/home/runner/work/boostsa/boostsa/boostsa', 'test_boot'])
 
 from boostsa import Bootstrap
 boot = Bootstrap()
