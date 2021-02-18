7e396a529f535262a81a55b6eca501afdb676d15
Tommaso Fornaciari <tommaso.fornaciari@gmail.com>
2021-02-18 22:38:35 +0100

    sblindo

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2021, Tommaso Fornaciari'
 author = 'Tommaso Fornaciari'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.18'
+release = '0.1.7'
 
 
 # -- General configuration ---------------------------------------------------
